61d624ae2d9a23379613a55d4e9cd0698b9b0a5a
SuperKingLol <laurence.stark11@gmail.com>
2017-12-08 16:08:09 +0000

    Remove old BoringProject, replace with new example project (#7)
    * Add C# project files
    
    * Delete BoringProjects, add example project
    
    Example of IP camera stream added. Beautiful user interface also included.

--- BoringTest/BoringTest/BoringTest.cpp
+++
@@ -1,9 +0,0 @@
-// An extremely boring test of the github repo with no other purpose
-#include <iostream>
-
-int main()
-{
-	std::cout << "Hello world!" << std::endl;
-
-	return EXIT_SUCCESS;
-}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Net.Http;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IpCamController _controller;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void ShowDragMove(object sender, MouseEventArgs e)
+        {
+            if (this.Cursor != Cursors.Wait)
+                Mouse.OverrideCursor = Cursors.ScrollAll;
+        }
+
+        private void ResetCursor(object sender, MouseEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void Clicker(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You have clicked the clicker!");
+        }
+
+        private void exitButton_click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _controller = new IpCamController("http://wmccpinetop.axiscam.net", "", "");
+            //_controller = new IpCamController("http://96.10.1.168", "","");
+            _controller.ImageReady += Dec_FrameReady;
+            _controller.StartProcessing();
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _controller.StopProcessing();
+        }
+
+        void Dec_FrameReady(object sender, ImageReadyEventArgs args)
+        {
+            imgStream.Source = args.Image;
+        }
+    }
+}
